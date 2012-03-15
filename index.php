47998946ea1063004d671590558ee50c8e934538
Ben Meissner <bmeissner@dkyinc.com>
2012-03-14 20:13:53 -0500

    Added reset
    added css to head of index file

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 		<meta charset="utf-8"/>
 		
 		<title>DKY Sandbox</title>
+		<link rel="stylesheet" href="css/reset.css" />
+		<link rel="stylesheet" href="css/style.css" />
 	</head>
 	<body>
 		<h1>DKY Sandbox</h1>
